bcfaf16fdc4116d5e4c22eb256da673e9f71dc69
d34dHusky <d34dHusky@users.noreply.github.com>
2022-09-26 20:26:53 +0200

    ClassLibrary changes

--- ISWProject/ClassLibrary/Class1.cs
+++ ISWProject/ClassLibrary/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace ClassLibrary
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
 {
     public class Class1
     {
-
     }
 }
