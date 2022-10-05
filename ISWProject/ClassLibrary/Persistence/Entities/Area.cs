7ae76773a0c3f01f68cff08e0a6b60229a037b3e
ivanhaaro <ivanharoliminana@outlook.es>
2022-10-05 11:01:22 +0200

    clase Area

---
+++ ISWProject/ClassLibrary/Persistence/Entities/Area.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Magazine.Entities
+{
+    public partial class Area
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual User Editor
+        {
+            get;
+            set;
+        }
+        public virtual Magazine Magazine
+        {
+            get;
+            set;
+        }
+        public virtual ICollection<Paper> Papers
+        {
+            get;
+            set;
+        }
+        public virtual ICollection<Paper> EvaluationPending
+        {
+            get;
+            set;
+        }
+        public virtual ICollection<Paper> PublicationPending
+        {
+            get;
+            set;
+        }
+
+    }
+}
