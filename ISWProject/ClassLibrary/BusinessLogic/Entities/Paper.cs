de79d0d5f3b6cc057121cadf28692f1c3d5b497f
Peeybol <Peeybol@users.noreply.github.com>
2022-10-05 11:01:08 +0200

    Clase Paper (falta acabar)

---
+++ ISWProject/ClassLibrary/BusinessLogic/Entities/Paper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Magazine.Entities
+{
+    public partial class Paper
+    {
+
+    }
+}
---
+++ ISWProject/ClassLibrary/Persistence/Entities/Paper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Magazine.Entities
+{
+    public partial class Paper
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public DateTime UpldoadDate
+        {
+            get;
+            set;
+        }
+
+        public virtual Evaluation Evaluation //No funciona pq no esta la clase Evaluation
+        {
+            get;
+            set;
+        }
+
+        public virtual Person CoAuthors
+        {
+            get;
+            set;
+        }
+
+        public virtual User Responsible
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
