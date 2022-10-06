b73fe7de8f6023e8e50a776de9ade4a047671d29
nachommarti <ilmarmar@etsinf.upv.es>
2022-10-06 18:07:27 +0200

    Issue

---
+++ ISWProject/ClassLibrary/BusinessLogic/Entities/Issue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Magazine.Entities
+{
+    public partial class Issue
+    {
+        public Issue()
+        {
+            PublishedPapers = new List<Paper>();
+        }
+
+        public Issue (int Id, int Number, DateTime PublicationDate, Magazine magazine):this()
+        {
+            this.Id = Id;
+            this.Number = Number;
+            this.PublicationDate = PublicationDate;
+            this.Magazine = Magazine;
+        }
+    }
+}
---
+++ ISWProject/ClassLibrary/Persistence/Entities/Issue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Magazine.Entities
+{
+    public partial class Issue
+    {
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime PublicationDate { get; set; }   
+
+        public ICollection<Paper> PublishedPapers { get; set; }
+
+        public Magazine Magazine { get; set; }
+    }
+}
