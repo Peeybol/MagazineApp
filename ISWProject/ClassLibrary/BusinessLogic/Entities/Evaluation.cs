1a6da29534633417fa72afe7d68e4f8211de3d59
ivanhaaro <ivanharoliminana@outlook.es>
2022-10-06 17:55:18 +0200

    Class Evaluation

---
+++ ISWProject/ClassLibrary/BusinessLogic/Entities/Evaluation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Magazine.Entities
+{
+    public partial class Evaluation
+    {
+        public Evaluation()
+        {
+            Paper = new Paper();
+        }
+        public Evaluation(Boolean Accepted, String Comments, DateTime Date, int Id):this()
+        {
+            this.Accepted = Accepted;
+            this.Comments = Comments;
+            this.Date = Date;
+            this.Id = Id;
+        }
+
+    }
+}
