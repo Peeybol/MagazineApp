cd718470c73eee75d5d90fe385f12e99d8617e55
Peeybol <Peeybol@users.noreply.github.com>
2022-10-06 17:47:42 +0200

    Clase Paper

---
+++ ISWProject/ClassLibrary/BusinessLogic/Entities/Paper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Magazine.Entities
+{
+    public partial class Paper
+    {
+        public Paper()
+        {
+            Evaluation = new Evaluation();
+            CoAuthors = new ICollection<Person>;
+            Responsible = new User();
+            Issue = new Issue();
+            BelongingArea = new Area();
+            EvaluationPendingArea = new Area();
+            PublicationPendingArea = new Area();
+        }
+
+        public Paper(int id, string title, DateTime uploadTime):this() {
+            Id = id;
+            Title = title;
+            UploadTime = uploadTime;
+        }
+    }
+}
